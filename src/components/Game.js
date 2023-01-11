12c0fb6acdc9456a21c8f0c25156dbff2295b9d3
Indumathi <indumathi@Indumathis-MacBook-Pro.local>
2023-01-11 15:25:25 +0530

    New UI add

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,17 +1,14 @@
 import React, { useReducer } from "react";
 import Board from "./Board";
-import { RiArrowGoBackLine } from "react-icons/ri";
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "JUMP":
-      debugger;
-      console.log("history..", state, action);
+    case "NEW":
       return {
-        ...state,
-        xIsNext: action.payload.step % 2 === 0,
-        history: state.history.slice(0, action.payload.step + 1),
+        xIsNext: true,
+        history: [{ squares: Array(9).fill(null) }],
       };
+
     case "MOVE":
       return {
         ...state,
@@ -19,6 +16,19 @@ const reducer = (state, action) => {
           squares: action.payload.squares,
         }),
         xIsNext: !state.xIsNext,
+      };
+
+    case "BACK":
+      return {
+        ...state,
+        xIsNext: action.payload.step % 2 === 0,
+        history: state.history.slice(0, action.payload + 1),
+      };
+    case "FORWARD":
+      return {
+        ...state,
+        xIsNext: action.payload.step % 2 === 0,
+        history: state.history.concat(action.payload.squares),
       };
 
     default:
@@ -33,10 +43,6 @@ export default function Game() {
   });
 
   const { xIsNext, history } = state;
-
-  const jumpTo = (step) => {
-    dispatch({ type: "JUMP", payload: { step } });
-  };
 
   const handleClick = (i) => {
     const current = history[history.length - 1];
@@ -54,8 +60,6 @@ export default function Game() {
 
   const winner = calculateWinner(current.squares);
 
-  console.log("state..", state.history.length);
-
   const status = winner
     ? winner === "D"
       ? "Draw"
@@ -64,39 +68,50 @@ export default function Game() {
     ? "First player is X"
     : "Next player is " + [xIsNext ? "X" : "O"];
 
-  const moves = history.map((step, move) => {
-    const steps =
-      state.history.length === 1 ? "Start the Game" : "Go to step: " + move;
-    return (
-      <div className="flex gap-x-4">
-        <li key={move}>
-          <button
-            onClick={() => jumpTo(move)}
-            className="font-medium text-base"
-          >
-            {steps}
-          </button>
-        </li>
-      </div>
-    );
-  });
+  const moveBack = history.length - 2;
 
   return (
-    <div
-      className={` w-full h-screen bg-gradient-to-r bg-cyan-400  flex justify-center items-center`}
-    >
-      <div className="flex  gap-x-6 ">
-        <Board
-          squares={current.squares}
-          onClick={(i) => {
-            handleClick(i);
-          }}
-          winner={winner}
-        />
+    <div className="h-screen flex  justify-center items-center bg-blue-200 gap-y-20">
+      <div className="flex flex-col gap-y-12">
+        <div className="text-6xl font-bold  text-blue-700 ">
+          Tic - Tac - Toe
+        </div>
+        <div className="flex gap-x-10 md:flex-row md:flex flex-col">
+          <Board
+            squares={current.squares}
+            onClick={(i) => {
+              handleClick(i);
+            }}
+            winner={winner}
+          />
+          <div className="flex mt-10 md:mt-4 gap-x-4 justify-center ">
+            <div className="font-medium text-xl italic ">{status}</div>
+            {state.history.length === 1 && (
+              <div className="underline text-xl font-medium">Start the Game</div>
+            )}
 
-        <div className="flex gap-x-8">
-          <div className="font-medium text-xl">{status}</div>
-          <ul>{moves}</ul>
+            {!winner && state.history.length !== 1 && (
+              <div
+                className="h-8 w-16 border-2 border-gray-600 rounded-lg shadow-lg px-2 bg-slate-100 cursor-pointer "
+                onClick={() => {
+                  dispatch({ type: "BACK", payload: moveBack });
+                }}
+              >
+                Undo
+              </div>
+            )}
+
+            {winner && (
+              <div
+                className="h-8 border-2 border-gray-600 rounded-lg shadow-lg px-2 bg-slate-100 text-wrap"
+                onClick={() => {
+                  dispatch({ type: "NEW" });
+                }}
+              >
+                Start the New Game
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import letterX from "../../src/letterX.png";
 import letterO from "../../src/letterO.png";
 
 export default function Square(props) {
   const sqaure =
     props.value === null ? null : props.value === "X" ? (
-      <img src={letterX} />
+      <img src={letterX} alt="X" />
     ) : (
-      <img src={letterO} />
+      <img src={letterO} alt="O" />
     );
   const [status, setStatus] = useState(false);
 
   return (
     <div onClick={() => (props.winner ? setStatus(true) : setStatus(false))}>
       <button
-        className={`w-28 h-28 bg-gradient-to-r gap-x-2 gap-y-2 text-base shadow-lg rounded-lg border-4 border-sky-700 ${
+        className={`w-28 h-28 bg-gradient-to-r gap-x-2 gap-y-2 text-base shadow-lg rounded-lg border-4 border-blue-600 ${
           props.winner
             ? "bg-gray-200 cursor-not-allowed"
-            : "bg-white hover:bg-pink-200 cursor-pointer"
+            : "bg-slate-100 hover:bg-pink-200 cursor-pointer"
         }`}
         onClick={props.onClick}
       >
